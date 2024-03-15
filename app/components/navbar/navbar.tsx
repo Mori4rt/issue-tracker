fd4f8448b86d233d376d5258050823fff2c08730
Madina <madina2005pulotova@gmail.com>
2024-03-15 18:46:58 +0700

    Style the active link in the navbar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,29 +1,41 @@
-import Link from 'next/link';
-import React from 'react';
-import { AiFillBug } from 'react-icons/ai';
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import classNames from 'classnames';
+import { usePathname } from 'next/navigation'
+import { GiAlienBug } from 'react-icons/gi'
 
-const NavBar = () => {
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues" }
-  ]
-  return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 py-5 items-center">
-      <Link href="/"><AiFillBug /></Link>
-      <ul className="flex space-x-6">
-        { links.map(link =>
-          <li key={link.href}>
-            <Link
-              className="text-zinc-500 hover:text-zinc-800 transition-colors"
-              href={link.href}
-            >
-              {link.label}
-            </Link>
-          </li>
-        )}
-      </ul>
-    </nav>
-  )
+
+const Navbar = () => {
+
+    const currentPath = usePathname();
+
+    const links = [
+        {label: "Dashboard", href: "/"},
+        {label: "Issues", href: "/issues"}
+    ]
+
+    return (
+        <nav className='flex space-x-8 border-b mb-5 py-5 items-center'>
+            <Link href='/'><GiAlienBug size="2em"/></Link>
+            <ul className='flex space-x-6'>
+                {links.map(link => 
+                    <li key={link.href}>
+                        <Link 
+                            href={link.href} 
+                            className = {classNames({
+                                'text-zinc-900': link.href === currentPath,
+                                'text-zinc-500': link.href !== currentPath,
+                                'hover:text-zinc-800 transition-colors': true
+                            })}
+                        > 
+                            {link.label} 
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
 }
 
-export default NavBar
+export default Navbar
