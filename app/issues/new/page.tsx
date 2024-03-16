3baa1f4f3763f5adb39785977ea17a3a29d11b47
Madina <madina2005pulotova@gmail.com>
2024-03-16 11:04:50 +0700

    Add markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+'use client'
 
+import React from 'react'
+import dynamic from 'next/dynamic' 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {ssr: false})
+import "easymde/dist/easymde.min.css";
 const NewIssuePage = () => {
   return (
     <div className='flex flex-col gap-y-5 max-w-xl' >
         <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-        <textarea className="textarea textarea-bordered" placeholder="Bio"></textarea>
+
+        <SimpleMDE 
+            options={{spellChecker:false}}
+            className="textarea textarea-bordered" placeholder="Bio">
+        </SimpleMDE>
         <button className='btn btn-primary w-fit'> 
-            add
+            Тыкни
       </button>
     </div>
   )
